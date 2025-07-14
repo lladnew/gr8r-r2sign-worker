26da77c907a6700aab7b440815d785eb8e85ee57
Chad Mowery <chad.mowery@gr8terthings.com>
2025-07-14 17:03:16 -0400

    fix: fully bundle AWS SDK for Cloudflare

--- build.js
+++ build.js
@@ -1,11 +1,10 @@
 import { build } from "esbuild";
 
 build({
-    entryPoints: ["src/index.js"],
-    bundle: true,
-    outfile: "dist/index.mjs",
-    format: "esm",
-    platform: "browser",
-    target: "es2022",
-    external: ["@aws-sdk/client-s3", "@aws-sdk/s3-request-presigner"],
+  entryPoints: ["src/index.js"],
+  bundle: true,
+  outfile: "dist/index.mjs",
+  format: "esm",
+  platform: "browser",
+  target: "es2022"
 }).catch(() => process.exit(1));
